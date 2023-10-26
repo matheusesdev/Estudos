9b5eb4ea40b1f49c4d2499b32f9e01cf9c43acc2
Matheus Santos <matheuses18@icloud.com>
2023-10-26 18:40:06 -0300

    Adicionadas operações aritméticas

--- Introdução a JavaScript/2_Operadores/js/script.js
+++ Introdução a JavaScript/2_Operadores/js/script.js
@@ -2,3 +2,8 @@
 console.log(typeof 2);
 console.log(typeof 5.14);
 console.log(typeof -50);
+// 2. Operações aritimeticas 
+console.log(2 + 4);
+console.log(10 - 5);
+console.log(5 * 4);
+console.log(10 / 5);

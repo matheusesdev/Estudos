cf342c73abab3a8f75ecdc98afc706783fef1f5b
Matheus Santos <matheuses18@icloud.com>
2023-10-26 20:26:07 -0300

    Update script.js

--- Introdução a JavaScript/2_Operadores/js/script.js
+++ Introdução a JavaScript/2_Operadores/js/script.js
@@ -1,7 +1,10 @@
+/*
 // 1- Number
 console.log(typeof 2);
 console.log(typeof 5.14);
 console.log(typeof -50);
+
+
 // 2. Operações aritimeticas 
 console.log(2 + 4);
 console.log(10 - 5);
@@ -9,3 +12,48 @@ console.log(5 * 4);
 console.log(10 / 5);
 
 console.log(5  +4 * 12)
+
+// 3. Special numbers.
+console.log(typeof Infinity);
+
+console.log(typeof -Infinity);
+
+console.log(12 * "asdf");
+
+console.log(typeof NaN);
+
+//4. Strings
+console.log("Hellow World!");
+console.log('Mais um Hello World');
+console.log('13');
+//Pode usar typeof nesses casos.
+console.log(typeof "Hellow World!");
+console.log(typeof 'Mais um Hello World');
+console.log(typeof '13'); */
+
+// 5. Simbolos especiais em string.
+console.log("Testando a \n quebra de linha")
+
+console.log("Espaçamento de \t tab ")
+
+
+// 6. Concatenação
+console.log(" Oi, " + " tudo " + " bem ?");
+
+//7. Interpolação   
+
+console.log(`soma de 2+2 é: ${2 + 2}`);
+
+console.log(`Podemos executar quaquer coisa aqui ${console.log("Teste")}`)
+
+// 8. Boolean
+console.log(true);
+console.log(5 > 3);
+console.log(2 < 9);
+console.log(2 > 9);
+console.log(typeof false);
+
+
+
+
+

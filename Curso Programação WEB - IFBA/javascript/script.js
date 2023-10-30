1395d907d9ecbf6b488ea569f77e8ba40f495ac7
Matheus Santos <matheuses18@icloud.com>
2023-10-30 12:09:08 -0300

    Alterações no HTML, CSS e JavaScript
    Estilização do contador. Criado os botões de adição, subtração e reset.
    Dentro do JavaScript foram criadas as funções de Adição, Subtração e Reset do contador.

---
+++ Curso Programação WEB - IFBA/javascript/script.js
@@ -0,0 +1,17 @@
+const sum = () =>{
+    const number = document.getElementById('number')
+    number.innerText ++;
+    return number.innerText;
+    
+}
+
+function subtraction (){
+    const number = document.getElementById('number')
+    number.innerText --;
+    return number.innerText;
+}
+function reset (){
+    const reset = document.getElementById('reset')
+    number.innerText = 0;
+    return number.innerText;
+}

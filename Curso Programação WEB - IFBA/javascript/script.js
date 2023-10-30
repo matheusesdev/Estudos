59e37d8212955ec45b8a433f7f9251dfb9e60bf3
Matheus Santos <matheuses18@icloud.com>
2023-10-30 16:27:27 -0300

    Alterações no HTML, CSS e JavaScript
    Foi acrescentado no HTML o botão "toggleBtn" com a função de contar apenas os números positivos.
    Foi feita a estilização no novo botão.
    E também foram adicionadas novas funções dentro do JavaScript.

--- Curso Programação WEB - IFBA/javascript/script.js
+++ Curso Programação WEB - IFBA/javascript/script.js
@@ -1,18 +1,83 @@
+let allowNegative = true;
 const sum = () =>{
     const number = document.getElementById('number')
     number.innerText ++;
-    return number.innerText;
-    
+
+    if(hasPositive(number.innerText)){
+        number.style.color = 'green'
+}
+
+applyParOrImpar(number.innerText)
+
+if(Number(number.innerText) === 0){
+    number.style.color = 'black'
+    document.getElementById('text-number').style.display = 'none'
+}
 }
 
 function subtraction (){
     const number = document.getElementById('number')
+    if(allowNegative || canSubtract(number.innerText)){
     number.innerText --;
-    return number.innerText;
+
+    if (hasNegative(number.innerText)){
+        number.style.color = 'red'   
 }
+
+
+applyParOrImpar(number.innerText)
+
+if(Number(number.innerText) == 0) {
+    number.style.color = 'black'
+    document.getElementById('text-number').style.display = 'none'
+}
+    }
+}
+function canSubtract(number){
+    return number > 0;
+}
+
+function hasPositive(number){
+    return (number < 0 ? false : true);
+}
+
+function hasNegative(number){
+    return (number > 0 ? false : true);
+}
+
+function applyParOrImpar(number){
+    if(hasPar(number)){
+        document.getElementById('text-number').innerText = number + ' é um  número par'
+        document.getElementById('text-number').style.display = 'block'
+
+    } else{
+        document.getElementById('text-number').style.display='none'
+    }
+}
+function hasPar(number){
+    return Number(number) % 2 === 0 ? true : false;
+}
+
+function toggleNegative(){
+    const number = document.getElementById('number');
+    allowNegative = !allowNegative;
+
+    if(!allowNegative && Number (number.innerText) < 0 ){
+        document.getElementById('text-number').style.display = 'none'
+        number.style.color = 'black'
+        number.innerText = 0;
+    }
+   const toggleNegativeBtn = document.getElementById('toggleBtn');
+   toggleNegativeBtn.classList.toggle('active', !allowNegative);
+}
+
+
+
 function reset (){
     const reset = document.getElementById('reset')
     number.innerText = 0;
-    return number.innerText;
+    number.style.color = 'black';
+    document.getElementById('text-number').style.display = 'none';
+    
 }
 

8b0bb9b405fdfba47649a3174d7e5d54c0c8f476
Matheus Santos <matheuses18@icloud.com>
2023-11-02 10:55:21 -0300

    Lógica de Programação
    Conceitos.

---
+++ Lógica de Programação/script.js
@@ -0,0 +1,43 @@
+
+
+
+// Algoritmo
+if (10 % 2 === 0) {
+  return "é Par";
+} else {
+  return "é ímpar";
+}
+
+/* Objetivo: O algoritmo tem como objetivo verificar se o número 10 é par ou ímpar.
+     
+    Processamento:
+    1. Utiloza o operador de módulo para verificar se o resto da divisão de 10 por 2 é igual a 0.
+    2. Compara se o resto da divisão é igual a 0.
+    
+    Decisão (Estrutura condicional):
+    1. Se o resto da divisão for igual a 0, o número é par (mensagem "é Par").
+    2. Se o resto da divisão for diferente de 0, o número é ímpar (mensagem "é ímpar").
+
+    Retorno:
+    1. Retorna a mensagem "é Par" ou "é ímpar".
+
+    Em resumo, o código é uma implementação de um agoritmo que decide se um número é par ou ímpar com base em um teste condicional simples. Essa sequência lógica de instruções representa a essência de um algoritmo, fornecendo uma solução clara e definida para um problema especifico. 
+*/
+
+
+//Variáveis e tipos de dados:
+
+//Variável para armazenar um número inteiro
+
+let numeroInteiro = 10; // Declara uma variável chamada numeroInteiro e atribui a ela o valor 10, que é um número inteiro.
+
+//Variável para armazenal um número decimal
+
+let numeroDecimal = 2.3; //Declara uma variável chamada numeroDecimal e atribui a ela o valor 2.3, que é um número decimal.
+
+//Variável para armazenar uma string
+
+let texto = "Olá, mundo!"; // Declara uma variável chamada texto e atribui a ela a string "Olá, mundo!"
+
+// Este código ilustra o uso de variáveis para armazenar diferentes tipos de dados, como números inteiros, decimais e strings, conforme mencionado no texto.
+

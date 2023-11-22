02959f19a691df7a9a7b52bf370f5cbd6cee4bf7
Matheus Santos <144345022+matheusesdev@users.noreply.github.com>
2023-11-22 14:36:31 -0300

    Update productCrud.js

--- CRUD/js/productCrud.js
+++ CRUD/js/productCrud.js
@@ -17,6 +17,24 @@ class ProductCrud{
     getProductById(id){
         return this.products.find(product => Number(product.id) === Number(id));
     }
+
+    //Editar um produto por ID.
+    
+    updateProduct(id, updateProduct){
+        //Encontrar o índice do produto no array.
+        const index = this.products.findIndex(product => Number.MAX_VALUE(product.id) === Number(id));
+
+        //Se o retorno for diferente de -1, ou seja, se o produto existir, atualiza o produto.
+        if(index != -1){
+            //Sobrescreve o produto original com os novos valores.
+            this.products[index] = {...this.products[index], ...updateProduct}
+        }
+
+    }
+
+
+
+
 }
 
 export {ProductCrud}

6695bf63fc0f790ff5195a7db18d8efa83710eb0
Matheus Santos <144345022+matheusesdev@users.noreply.github.com>
2023-12-18 14:17:40 -0300

    update
    Criação do modal, update app.js e productCrud.js

--- CRUD/js/app.js
+++ CRUD/js/app.js
@@ -1,110 +1,156 @@
-import { ProductCrud } from "./productCrud.js";
+// app.js
 
+// import { ProductCrud } from './productCrud.js';
+import { ProductCrud } from './productCrud.js';
 const productCrud = new ProductCrud();
-
-console.log("MEUS PRODUTOS: ", productCrud.products);
+const NUMBER_WHATSAPP = '5577988888888';
 
 function addProduct() {
-  const productName = document.getElementById("productName").value;
-  const productPrice = document.getElementById("productPrice").value;
-  const productQtd = document.getElementById("productQtd").value;
+    const productName = document.getElementById('productName').value;
+    const productPrice = parseFloat(document.getElementById('productPrice').value);
+    const productQtd = parseInt(document.getElementById('productQtd').value);
 
-  // Verifica se o nome do produto e o preço são válidos
-  if (productName && !isNaN(productPrice) && productQtd) {
-    const newProduct = {
-      id: productCrud.generateProductId(),
-      name: productName,
-      price: Number(productPrice),
-      qtdEstoque: Number(productQtd),
-    };
+    // Verifica se o nome do produto e o preço são válidos
+    if (productName && !isNaN(productPrice) && productQtd) {
+        const newProduct = {
+            id: generateProductId(),
+            name: productName,
+            price: productPrice,
+            qtdEstoque: productQtd,
+        };
 
-    // Adiciona o novo produto à lista
-    productCrud.addProduct(newProduct);
+        // Adiciona o novo produto à lista
+        productCrud.addProduct(newProduct);
 
-    // Atualiza a tabela
-    updateTable();
+        // Atualiza a tabela
+        updateTable();
 
-    //Limpa o formulário
-    document.getElementById("productName").value = "";
-    document.getElementById("productPrice").value = "";
-    document.getElementById("productQtd").value = "";
-  } else {
-    alert("Por favor, preencha todos os campos!");
-  }
+        // Limpa o formulário
+        document.getElementById('productName').value = '';
+        document.getElementById('productPrice').value = '';
+        document.getElementById('productQtd').value = '';
+    } else {
+        alert('Por favor, insira um nome de produto válido e um preço numérico.');
+    }
 }
 
-function updateProduct() {
-  console.log("Atualizar produto");
+// Função para gerar um ID único para o produto
+function generateProductId() {
+    return productCrud.generateProductId();
 }
 
-//Função para exlcluir um produto.
-function deleteProduct(productId) {
-  const confirmDelete = confirm(
-    `Tem certeza que deseja excluir o registro de ID: ${productId} ?`
-  );
-  if (confirmDelete) {
-    productCrud.deleteProduct(productId);
-    
-    //Atualiza a lista contendo o novo produto
-    updateTable()
-  }
-  console.log("excluir produto: ", productId);
-}
-
-//Função para atualizar e listar tabela com produtos.
+// Função para atualizar a tabela com os produtos
 function updateTable() {
-  const tableBody = document.querySelector("#productTable tbody");
+    const tableBody = document.querySelector('#productTable tbody');
+    tableBody.innerHTML = '';
 
-  //Garantir que a tabela esteja vazia
-  tableBody.innerHTML = "";
+    // Obtém todos os produtos
+    const products = productCrud.getAllProducts();
 
-  //Obter todos os produtos.
-  const products = productCrud.getAllProducts();
-  products.forEach((product) => {
-    const row = document.createElement("tr");
-
-    //Formatar para unidade monetária BRL.
-    const formatter = new Intl.NumberFormat("pt-Br", {
-      style: "currency",
-      currency: "BRL",
+    // Preenche a tabela com os produtos
+    const formatter = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
     });
 
-    row.innerHTML = `
+    products.forEach((product) => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
         <td>${product.id}</td>
         <td>${product.name}</td>
         <td>${formatter.format(product.price)}</td>
         <td>${product.qtdEstoque}</td>
         <td id="actions">
-            <button type ="button" onclick="updateProduct(${
-              product.id
-            })">Editar</button>
-            <button class="deleteBtn" data-product-id="${
-              product.id
-            }" type ="button">Excluir</button>
+          <button type="button" class="openModalBtn" data-product-id="${product.id}" >Editar</button>
+          <button type="button" class="deleteBtn" data-product-id="${product.id}">Excluir</button>
         </td>
-    `;
-    tableBody.appendChild(row);
-  });
+      `;
+        tableBody.appendChild(row);
+    });
 }
 
+// Função para editar um produto
+function editProduct(productId, product) {
+    if (product.updateName && !isNaN(product.updatePrice) && product.updateQtdEstoque) {
+        productCrud.updateProduct(productId, {
+            name: product.updateName,
+            price: product.updatePrice,
+            qtdEstoque: product.updateQtdEstoque
+        });
+        updateTable();
+    } else {
+        alert('Por favor, insira um nome de produto válido e um preço numérico.');
+    }
+}
+
+// Função para excluir um produto
+function deleteProduct(productId) {
+    const confirmDelete = confirm('Tem certeza de que deseja excluir este produto?');
+
+    if (confirmDelete) {
+        productCrud.deleteProduct(productId);
+        updateTable();
+    }
+}
+
+// Inicializa a tabela com os produtos existentes
 updateTable();
 
-const btnForm = document.getElementById("productForm");
-btnForm.addEventListener("submit", function (event) {
-  // Previne o envio padrão do formulário
-  event.preventDefault();
-
-  // Chama a função addProduct diretamente do escopo do módulo
-  addProduct();
+const btnForm = document.getElementById('productForm');
+btnForm.addEventListener('submit', function (event) {
+    // Previne o envio padrão do formulário
+    event.preventDefault();
+    // Chama a função addProduct diretamente do escopo do módulo
+    addProduct();
 });
 
-const actions = document.getElementById("productTable");
-let productId;
+const actions = document.getElementById('productTable')
+const updateProductBtn = document.getElementById("updateProductBtn");
 
-actions.addEventListener("click", function (event) {
-  const target = event.target;
-  if (target.classList.contains("deleteBtn")) {
-    productId = target.dataset.productId;
-    deleteProduct(productId);
-  }
+let productId;  // Variável para armazenar o ID do produto
+
+// Evento para acionar métodos de deletar e atualizar registro
+actions.addEventListener('click', function (event) {
+    const target = event.target;
+
+    if (target.classList.contains('deleteBtn') || target.classList.contains('openModalBtn')) {
+        productId = target.dataset.productId;
+
+        if (target.classList.contains('deleteBtn')) {
+            deleteProduct(productId);
+        } else if (target.classList.contains('openModalBtn')) {
+            insertModalFields()
+            const modal = document.querySelector('.modal');
+            modal.style.display = "block";
+        }
+    }
+
+    updateProductBtn.addEventListener('click', function () {
+        const productNameInput = document.getElementById("updateProductName");
+        const productPriceInput = document.getElementById("updateProductPrice");
+        const productQtdInput = document.getElementById("updateProductQtd");
+
+        editProduct(productId, {
+            updateName: productNameInput.value,
+            updatePrice: productPriceInput.value,
+            updateQtdEstoque: productQtdInput.value
+        });
+        updateModal.style.display = "none";
+    });
+});
+
+// Função para preencher os campos do modal
+function insertModalFields() {
+    const product = productCrud.getProductById(productId);
+    document.getElementById("updateProductName").value = product.name;
+    document.getElementById("updateProductPrice").value = product.price;
+    document.getElementById("updateProductQtd").value = product.qtdEstoque;
+}
+
+// Função para fechar modal
+const modal = document.querySelector('.modal');
+modal.addEventListener('click', function (event) {
+    if (event.target === modal || event.target.classList.contains('close')) {
+        modal.style.display = "none";
+    }
 });
--- CRUD/js/productCrud.js
+++ CRUD/js/productCrud.js
@@ -1,83 +1,74 @@
 class ProductCrud {
   constructor() {
+    // Array estático para armazenar produtos
     this.products = [
       {
-        id: this.generateProductId() ,
-        name: 'Ração Premium 50KG ',
-        price: 14.94 ,
+        id: this.generateProductId(),
+        name: 'Ração Premium 50KG',
+        price: 295.8,
         qtdEstoque: 10
-      }, 
+      },
       {
-        id: this.generateProductId() ,
+        id: this.generateProductId(),
         name: 'Ração Gold 50KG',
-        price: 250.5 ,
+        price: 250.5,
         qtdEstoque: 20
-      }, 
-
+      },
       {
-        id: this.generateProductId() ,
-        name: 'Coleira para Gato',
-        price: 30.70 ,
-        qtdEstoque: 44
-      }, 
-
+        id: this.generateProductId(),
+        name: 'Colera para Gato',
+        price: 25.5,
+        qtdEstoque: 5
+      },
       {
-        id: this.generateProductId() ,
+        id: this.generateProductId(),
         name: 'Shampoo para Pets',
-        price: 12.70 ,
-        qtdEstoque: 22
-      }, 
+        price: 15.7,
+        qtdEstoque: 0
+      },
       {
-        id: this.generateProductId() ,
-        name: 'Banheiro Inteligente',
-        price: 500.0,
-        qtdEstoque: 3
-      }, 
-
-
+        id: this.generateProductId(),
+        name: 'Banheiro Inteligente ',
+        price: 459.9,
+        qtdEstoque: 4
+      },
     ];
   }
 
-  //Adicionar um produto
+  // Adicionar um produto
   addProduct(product) {
     this.products.push(product);
   }
 
-  //Obter todos os produtos
+  // Obter todos os produtos
   getAllProducts() {
     return this.products;
   }
 
-  //Obter um produto por ID.
+  // Obter um produto por ID
   getProductById(id) {
-    return this.products.find((product) => Number(product.id) === Number(id));
+    return this.products.find(product => Number(product.id) === Number(id))
   }
 
-  //Editar um produto por ID.
-
-  updateProduct(id, updateProduct) {
-    //Encontrar o índice do produto no array.
-    const index = this.products.findIndex(
-      (product) => Number.MAX_VALUE(product.id) === Number(id)
-    );
-
-    //Se o retorno for diferente de -1, ou seja, se o produto existir, atualiza o produto.
-    if (index != -1) {
-      //Sobrescreve o produto original com os novos valores.
-      this.products[index] = { ...this.products[index], ...updateProduct };
+  // Editar um produto por ID
+  updateProduct(id, updatedProduct) {
+    // Encontrar o índice do produto no array
+    const index = this.products.findIndex(product => Number(product.id) === Number(id));
+    // Se o produto for encontrado, atualizar suas propriedades
+    if (index !== -1) {
+      this.products[index] = { ...this.products[index], ...updatedProduct };
     }
   }
 
-  //Remover um produto por ID.
-  deleteProduct(id){
-    return this.products = this.products.filter(product => Number(product.id) !== Number(id))
+  // Remover um produto por ID
+  deleteProduct(id) {
+    this.products = this.products.filter(product => Number(product.id) !== Number(id));
   }
 
-  //Função para gerar um ID único para o produto.
-  generateProductId(){
-    return Math.floor(Math.random() * 1000) + 1
+  // Função para gerar um ID único para o produto
+  generateProductId() {
+    return Math.floor(Math.random() * 1000) + 1;
   }
-
 
 }
 
